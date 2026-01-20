4a307e09835d03952fa35bc956c1d1a0b3835fa7
Julio Figueiredo <julioo.1amorim@gmail.com>
2026-01-20 12:14:58 -0300

    :sparkles: feat: create routes for upload and chat

---
+++ backend/routes/routes.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, UploadFile, File
+import shutil
+import os
+from backend.services.rag_engine import RagEngine
+from pydantic import BaseModel
+
+router = APIRouter()
+
+rag_engine = RagEngine()
+
+class QueryRequest(BaseModel):
+    question: str
+
+@router.post("/upload")
+def upload_file(file: UploadFile = File(...)):
+
+    os.makedirs("temp", exist_ok=True)
+
+    file_path = f"temp/{file.filename}"
+
+    # wb don't try to transform binary in text like 'w'
+    with open(file_path, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    rag_engine.process_document(file_path)
+
+    return {"filename": file.filename, "status": "Processed!"}
+
+@router.post("/chat")
+def chat_pdf(request: QueryRequest):
+
+    answer = rag_engine.answer(request.question)
+
+    return {
+        "question": request.question,
+        "answer": answer
+    }
